7293c5c6b514aad52409693387191ec357d6668b
pablo <stevaopabllo@gmail.com>
2025-03-01 13:36:31 -0300

    big-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BigCardComponent } from "./big-card/big-card.component";
 import { TitleComponent } from "./title/title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BigCardComponent, TitleComponent],
+  imports: [RouterOutlet, BigCardComponent, TitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../../title/title.component";
+import { BigCardComponent } from "../../big-card/big-card.component";
+import { SmallCardComponent } from "../../small-card/small-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TitleComponent],
+  imports: [TitleComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
