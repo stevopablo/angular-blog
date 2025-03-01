d110d37f77c07bc80f2c2eada110da026d77f480
pablo <stevaopabllo@gmail.com>
2025-03-01 13:02:44 -0300

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BigCardComponent } from "./big-card/big-card.component";
+import { TitleComponent } from "./title/title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BigCardComponent, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
