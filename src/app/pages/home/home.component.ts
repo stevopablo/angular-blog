6cefa759a3f63a3828b93c1c4d8769dafda22d3d
pablo <stevaopabllo@gmail.com>
2025-03-01 14:58:49 -0300

    terminei

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TitleComponent } from "../../title/title.component";
 import { BigCardComponent } from "../../big-card/big-card.component";
 import { SmallCardComponent } from "../../small-card/small-card.component";
+import { HeaderComponent } from "../../header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TitleComponent, BigCardComponent, SmallCardComponent],
+  imports: [TitleComponent, BigCardComponent, SmallCardComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
