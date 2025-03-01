124c8cc238faa0b8cd1f1427de9b4211bdbd88c2
pablo <stevaopabllo@gmail.com>
2025-03-01 13:06:07 -0300

    pages

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from "../../title/title.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [TitleComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
